3f277e6bfb74c300444fd1bb1566b795dfd952ec
Filipe Herculano <fifo_@live.com>
2020-02-18 18:04:58 -0500

    Initial Commit

---
+++ clipbar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  clipbar
+//
+//  Created by Filipe Herculano on 2020-02-18.
+//  Copyright © 2020 Filipe Herculano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
